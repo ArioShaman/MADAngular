0bb0727f0314ed240ef8d89668212ca25f48f13b
ArioShaman <ariosha6@gmail.com>
2017-12-22 20:19:23 +0300

    setup slim sass and coffe

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,17 @@
+class CountriesController < ApplicationController
+  before_action :set_country, only: [:show]
+
+  # GET /countries
+  # GET /countries.json
+  def index
+    @countries = Country.all
+  end
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_country
+      @country = Country.find(params[:id])
+    end
+end
